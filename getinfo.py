2067f28e16878456a7da97a97fa1947d78b51962
lucasalvatore1 <32399029+lucasalvatore1@users.noreply.github.com>
2019-02-11 18:37:47 -0500

    Update getinfo.py

--- getinfo.py
+++ getinfo.py
@@ -13,7 +13,6 @@ with open('switchlist') as infile:
 			junos_driver = get_network_driver("junos")
 			device = junos_driver(hostname=host.strip(), username=username, password=password)
 			device.open()
-			#print("Working on:", host, end=" ")
 			print(device.get_facts()["hostname"],end=" ")
 			print(device.get_facts()["model"],end=" ")
 			print(device.get_facts()["serial_number"])
@@ -22,4 +21,3 @@ with open('switchlist') as infile:
 			print("Error:", e)
 		continue
 
-# device = driver("bbr1.ewr1.packet.net", username, password)
