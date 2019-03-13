1092725c93ecb6c962241a3f1d344b43092daff3
Luca Salvatore <luca@Lucas-MacBook-Pro.local>
2019-03-13 09:34:30 -0400

    added commands arg

--- netmiko-multiple-devices-show.py
+++ netmiko-multiple-devices-show.py
@@ -3,9 +3,7 @@ from getpass import getpass
 from sys import argv
 from netmiko import ConnectHandler
 
-script, devices, user_name = argv
-
-device_list = open(devices).read()
+script, devices, commands, user_name = argv
 
 password = getpass("Enter your password:")
 
@@ -18,19 +16,22 @@ with open(devices) as infile:
                 "password": password,
                 "device_type": "juniper_junos"
             }
-            #print(f'{host!r}')
-            #print(f"device = {host}")
-            #print(f"username = {user_name}")
-
             net_connect = ConnectHandler(**device)
-            command = "YOUR COMMANDS HERE"
-
-            print()
-            print(net_connect.find_prompt())
-            output = net_connect.send_command(command)
+            with open(commands) as c:
+                for line in c:
+                    try:
+                        #print(line)
+                        command = line.strip()
+                        print()
+                        print(net_connect.find_prompt())
+                        output = net_connect.send_command(command)
+                        #net_connect.disconnect()
+                        print(output)
+                        print()
+                    except Exception as e:
+                        print("Error:", e)
+                    continue
             net_connect.disconnect()
-            print(output)
-            print()
         except Exception as e:
             print("Error:", e)
         continue
